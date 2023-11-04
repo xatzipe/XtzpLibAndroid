4fa1a805c631e685350ee3ce74eab27207dea636
Theodore Chatziperis <xatzipe@gmail.com>
2023-11-04 02:12:47 +0200

    fix instrumentation test

--- XtzpLib/src/androidTest/java/io/github/xatzipe/xtzplib/ExampleInstrumentedTest.kt
+++ XtzpLib/src/androidTest/java/io/github/xatzipe/xtzplib/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.xatzipe.xtzplib.test", appContext.packageName)
+        assertEquals("io.github.xatzipe.xtzplib.test", appContext.packageName)
     }
 }
