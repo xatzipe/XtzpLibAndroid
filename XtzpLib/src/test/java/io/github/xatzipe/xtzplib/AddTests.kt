991fa613214c0935712d8966d78fbce624422158
Theodore Chatziperis <xatzipe@gmail.com>
2023-11-06 12:53:58 +0200

    added some tests. setup jacooc. added sonar scan

---
+++ XtzpLib/src/test/java/io/github/xatzipe/xtzplib/AddTests.kt
@@ -0,0 +1,13 @@
+package io.github.xatzipe.xtzplib
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AddTests {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, add(2, 2))
+    }
+}
---
+++ XtzpLib/src/test/java/io/github/xatzipe/xtzplib/MultiplyTests.kt
@@ -0,0 +1,13 @@
+package io.github.xatzipe.xtzplib
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MultiplyTests {
+
+    @Test
+    fun multiply_isCorrect() {
+        assertEquals(4, multiply(2, 2))
+    }
+}
